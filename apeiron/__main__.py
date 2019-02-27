b6e49091a6b2e99bda7287bb64e7865b896e3cf5
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2019-02-27 16:48:02 -0600

    updating values

---
+++ apeiron/__main__.py
@@ -0,0 +1,4 @@
+from apeiron import app
+
+if __name__ == '__main__':
+    app.run()
--- apeiron/app.py
+++ apeiron/app.py
@@ -1,14 +1,17 @@
 import sys
-sys.path.append('C:\\Users\\daniel.cortes\\development\\git\\apeiron-cli\\apeiron-cli\\reader')
+#sys.path.append('C:\\Users\\daniel.cortes\\development\\git\\apeiron-cli\\apeiron-cli\\reader')
 
-from apeiron.reader import add
+#from apeiron.reader import add
 
 from fpdf import FPDF
  
-#pdf = FPDF()
-#pdf.add_page()
-#pdf.set_font("Arial", size=12)
-#pdf.cell(200, 10, txt="Welcome to Python!", ln=1, align="C")
-#pdf.output("simple_demo.pdf")
+pdf = FPDF()
+pdf.add_page()
+pdf.set_font("Arial", size=12)
+pdf.cell(200, 10, txt="Welcome to Python!", ln=1, align="C")
+pdf.output("demo_pdf.pdf")
 
-print(add(2,3))
+#print(add(2,3))
+
+def run():
+    print('Saludos')
