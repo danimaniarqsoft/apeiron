603ffb6020ba6373b584b7239c9d87ed1697cbd4
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2019-04-16 18:50:16 -0500

    updating plugins configuration

--- apeiron/app.py
+++ apeiron/app.py
@@ -6,7 +6,7 @@ from pkg_resources import iter_entry_points
 import click
 from click_plugins import with_plugins
 
-@with_plugins(iter_entry_points('printit.plugins'))
+@with_plugins(iter_entry_points('apeirion.plugins'))
 @click.group()
 def cli():
     """
