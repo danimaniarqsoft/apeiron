8ad27b26886ee901b10a536c6f3e430309ed8dc2
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2019-04-26 22:57:19 -0500

    adding apeiron file config

--- apeiron/commons/apeiron.py
+++ apeiron/commons/apeiron.py
@@ -2,23 +2,24 @@
 # -*- coding: utf-8 -*-
 import click
 import os
+import json
 from pathlib import Path
 from apeiron.commons.message_type import MessageType
 from apeiron.commons.message import Message
 from apeiron.commons.file_version_manager import FileVersionManager
 import glob
+from datetime import datetime
+
 
 class Apeiron:
-
-    '''
-    FILE EXIST AND IS THE SAME -> not write and skipt, print warning
-    FILE EXIST AND IS DIFFERENT -> make a diff and not save it
-    FILE NOT EXIST -> just save it and print where is save it, return path
-    '''
 
     @staticmethod
     def init():
         Path(Apeiron.apeirondir()).mkdir(parents=True, exist_ok=True)
+        data = {'author':'apeiron','gendate':datetime.today().strftime('%Y%m%d')}  
+        with open(Apeiron.apeironconfig(), 'w+') as outfile:  
+            json.dump(data, outfile, indent=4)
+            Path(Apeiron.apeironconfig()).touch()
 
     @staticmethod
     def isapeiron():
@@ -30,3 +31,16 @@ class Apeiron:
     @staticmethod
     def apeirondir():
         return '.apeiron'
+
+    @staticmethod
+    def apeironconfig():
+        return Apeiron.apeirondir() + '/' + 'config.json' 
+
+    @staticmethod
+    def savestate(key, value):
+        with open(Path('.apeiron/config.json'), 'r+') as f:
+            data = json.load(f)
+            data[key] = value # <--- add `id` value.
+            f.seek(0)        # <--- should reset file position to the beginning.
+            json.dump(data, f, indent=4)
+            f.truncate()     # remove remaining part
