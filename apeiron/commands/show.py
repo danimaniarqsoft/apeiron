580f7cab51b57e9ec92ec3844fc56b1e6319cf7d
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2019-04-29 14:56:09 -0500

    adding contributing file

--- apeiron/commands/show.py
+++ apeiron/commands/show.py
@@ -5,6 +5,7 @@ Main Command Line Interface for Plugin Management
 """
 import os
 import click
+import difflib
 from pkg_resources import iter_entry_points
 from click_plugins import with_plugins
 from apeiron.commons import Message, Files, FileVersionManager
@@ -15,6 +16,14 @@ from apeiron.commons.help_messages import HelpMessages
 from pathlib import Path
 import glob
 
+def diff(file_path):
+    base_dir = file_path.parent
+    file_name = file_path.name
+    file_original = open(file_path).readlines()
+    file_conflict = open(base_dir / (file_name + '.conflict')).readlines()
+    for line in difflib.unified_diff(file_original, file_conflict):
+        print(line)
+    
 
 @click.group(help='Command to  add <components>')
 def show():
@@ -22,10 +31,14 @@ def show():
 
 
 @show.command()
-def conflicts():
-    files_finded = glob.iglob('**/*.conflict', recursive=True)
-    count = 0
-    for filename in files_finded:
-        Message.failure(Path(filename).with_suffix('').as_posix())
-        count = count + 1
-    Message.failure('Conflicts founded:', str(count))
+@click.option('--file')
+def conflicts(file):
+    if not file:
+        files_finded = glob.iglob('**/*.conflict', recursive=True)
+        count = 0
+        for file in files_finded:
+            Message.failure(Path(file).with_suffix('').as_posix())
+            count = count + 1
+        Message.failure('Conflicts founded:', str(count))
+    else:
+        diff(Path(file))
