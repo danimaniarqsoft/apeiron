2c0c3eb320fac3fe4f51eeb1a2f9b61ce7419341
daniel <arquitectura.conacyt@conacyt.mx>
2020-02-13 19:27:28 -0600

    update se mejora la  consulta de reportes

--- cvu.py
+++ cvu.py
@@ -1,51 +1,37 @@
 import click
-from zeep import Client, Settings
-from zeep.plugins import HistoryPlugin
 import requests
 from xml.etree import ElementTree
-from bs4 import BeautifulSoup
 from base64 import b64decode
-from util import request_factory
+from util import consultar_cvu, login
 import csv
-
 
 @click.command()
 def cvu():
-    url = 'http://vsmiic230:80/cvu/ws/reporteExternoCvu'
-    headers = {'x-auth-token': '40356181-a406-4df0-9629-6d18606b17da', 'content-type': 'text/xml'}
-    destination_path = '/home/daniel/Desktop/'
-    
+    context = {'token': login('gochihh@gmail.com','Rkk3NzMydW5AbQ=='), 'cvu': 0}
+    destination_path = '/home/daniel/Desktop/cvu/'
     with open('data.csv', mode='r') as infile:
         reader = csv.reader(infile)
         next(reader, None)  # skip the headers
         with open('result.csv', mode='w') as outfile:
             writer = csv.writer(outfile)
             writer.writerow(['cvu', 'status'])
-            for rows in reader:
-                cvu_number = rows[0]
-                request = request_factory(cvu_number)
-                resp = requests.post(url, data=request, headers=headers)
-                if resp.status_code != 200:
-                    print(resp.status_code)
-                    writer.writerow([cvu_number, 'fail'])
-                    print(cvu_number + ', fail')
+            for row in reader:
+                context['cvu']= row[0]
+                root = consultar_cvu(context)
+                file_exist = False
+                for repo in root.iter('archivo'):
+                    file_exist = True
+                    bytes = b64decode(repo.text, validate=False)
+                    file_name = destination_path + str(context['cvu']) + '.pdf' 
+                    f = open(file_name, 'wb')
+                    f.write(bytes)
+                    f.close()
+                if(file_exist):
+                    writer.writerow([context['cvu'], 'success'])
+                    print(context['cvu'] + ', success')
                 else:
-                    soup = BeautifulSoup(resp.content, features="lxml")
-                    root = ElementTree.fromstring(soup.prettify())
-                    file_exist = False
-                    for repo in root.iter('archivo'):
-                        file_exist = True
-                        bytes = b64decode(repo.text, validate=False)
-                        file_name = destination_path + str(cvu_number) + '.pdf' 
-                        f = open(file_name, 'wb')
-                        f.write(bytes)
-                        f.close()
-                    if(file_exist):
-                        writer.writerow([cvu_number, 'success'])
-                        print(cvu_number + ', success')
-                    else:
-                        writer.writerow([cvu_number, ', fail'])
-                        print(cvu_number + ', fail')
+                    writer.writerow([context['cvu'], ', fail'])
+                    print(context['cvu'] + ', fail')
 
 if __name__ == '__main__':
     cvu()
